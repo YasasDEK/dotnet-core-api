19d494f53245191e8cbbd68902ce56882ae9b0e6
YasasDEK <ydilshan.ek@gmail.com>
2023-04-03 16:13:26 +0530

    first commit

---
+++ TaskAPI/Controllers/TasksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Tasks 3" };
+            return Ok(tasks);  
+        }
+        
+        [HttpPost]
+        public IActionResult NewTasks()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTasks()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTasks()
+        {
+            return Ok();
+        }
+
+    }
+}
